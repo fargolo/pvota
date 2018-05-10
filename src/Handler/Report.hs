dd578ecfb45d297a973cd14b9d864fe0109fb538
pingfreud <pingfreud@protonmail.com>
2018-05-10 16:09:54 -0300

    Report embed

--- src/Handler/Report.hs
+++ src/Handler/Report.hs
@@ -5,9 +5,13 @@
 {-# LANGUAGE TypeFamilies #-}
 module Handler.Report where
 
+import Data.FileEmbed (embedFile)
 import Import
 
-getReportR :: Handler ()
-getReportR = do
-    let indexPath = "static/reports/outputs/descriptive-v3.html"
-    sendFile "text/html" indexPath
+-- getReportR :: Handler ()
+-- getReportR = do
+--    let indexPath = "static/reports/outputs/descriptive-v3.html"
+--    sendFile "text/html" indexPath
+getReportR :: Handler TypedContent
+getReportR = return $ TypedContent "text/html"  
+                    $ toContent $(embedFile "static/reports/outputs/descriptive-v3.htmo")
